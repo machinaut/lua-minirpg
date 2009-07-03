2b7580505decadd0b9e6b86af6142e8e0ba93acc
Your Name <ajray@ncsu.edu>
2009-07-03 02:54:16 +0000

    clone module

---
+++ clone.lua
@@ -0,0 +1,27 @@
+module(..., package.seeall);
+
+function clone( base_object, clone_object )
+  if type( base_object ) ~= "table" then
+    return clone_object or base_object 
+  end
+  clone_object = clone_object or {}
+  clone_object.__index = base_object
+  return setmetatable(clone_object, clone_object)
+end
+
+function isa( clone_object, base_object )
+  local clone_object_type = type(clone_object)
+  local base_object_type = type(base_object)
+  if clone_object_type ~= "table" and base_object_type ~= table then
+    return clone_object_type == base_object_type
+  end
+  local index = clone_object.__index
+  local _isa = index == base_object
+  while not _isa and index ~= nil do
+    index = index.__index
+    _isa = index == base_object
+  end
+  return _isa
+end
+
+
